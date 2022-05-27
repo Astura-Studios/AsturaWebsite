fa24a9b0e63645c514be7072d77ec292236cf62e
Ascendus <ascendus.dev@gmail.com>
2022-05-28 11:49:02 +1200

    fix: fixed tailwind issues

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export const Header: NextComponentType = (): JSX.Element => {
     return (
-        <div className="overflow-hidden bg-gray-900">
+        <div className="overflow-hidden bg-gray-900 h-screen">
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="flex flex-col items-center justify-between xl:flex-row">
                     <div className="w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12">
                         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
                             The quick, brown fox <br className="hidden md:block" />
-                            jumps over a{' '}
+                            jumps over a{" "}
                             <span className="text-teal-accent-400">lazy dog</span>
                         </h2>
                         <p className="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}"
+        "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    presets: [],
-    darkMode: "class",
     theme: {
         extend: {
             fontFamily: {
