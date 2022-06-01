7336c3c52d266815076306feb9167e2b7dd1b551
Ascendus <ascendus.dev@gmail.com>
2022-06-01 18:27:05 +1200

    refractor: removed unnecessary parentheses

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,9 @@ import { Page } from "../components/base";
 import metadata from "../lib/static/metadata.json";
 
 const Home: NextPage = (): JSX.Element => {
-    return (
-        <Page name="Home" meta={metadata["home"]}>
-            <p className="text-2xl">Hello, World!</p>
-        </Page>
-    );
+    return <Page name="Home" meta={metadata["home"]}>
+        <p className="text-2xl">Hello, World!</p>
+    </Page>;
 };
 
 export default Home;
